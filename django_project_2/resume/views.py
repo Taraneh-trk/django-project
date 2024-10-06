dcc76eb3dfa8defde35ed50bc986d6a36449d5cc
Taraneh <145665647+Taraneh-trk@users.noreply.github.com>
2024-10-06 16:46:00 +0330

    django_project_2 part3

--- django_project_2/resume/views.py
+++ django_project_2/resume/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def home_view(request):
-    return render(request,'index.html')
+    context = { 'name':'Taraneh' , 'lastname':'kordi' , 'phonenumber':'09876543212' }
+    return render(request,'index.html',context)
